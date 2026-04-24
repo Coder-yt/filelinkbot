19600b34638668ea97c9c3a8d3f5679f416e4ffa
MD-Developer-yt <animeworldday2005@gmail.com>
2026-04-24 21:06:02 +0530

    config.py
    Change OWNER_ID retrieval to raise an exception if not set.

--- config.py
+++ config.py
@@ -7,5 +7,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 
 BOT_USERNAME = os.getenv("BOT_USERNAME")  # without @
-OWNER_ID = int(os.getenv("OWNER_ID"))
+OWNER_ID = os.getenv("OWNER_ID")
+if OWNER_ID:
+    OWNER_ID = int(OWNER_ID)
+else:
+    raise Exception("OWNER_ID is not set in environment variables")
 PORT = int(os.getenv("PORT", 10000))
