30e93034631eab1429792704f6cd84f993733252
Coder-yt <animeupdatesau04@gmail.com>
2026-08-08 17:51:03 +0530

    database.py

--- database.py
+++ database.py
@@ -605,18 +605,3 @@ async def total_ratings():
         }
     )
 
-# ==========================================
-# OWNER / ADMIN CHECK
-# ==========================================
-
-async def is_owner_or_admin(user_id):
-
-    if user_id == OWNER_ID:
-        return True
-
-    try:
-        return await is_admin(user_id)
-    except Exception:
-        return False
-
-
